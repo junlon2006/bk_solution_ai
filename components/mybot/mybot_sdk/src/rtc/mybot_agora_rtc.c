71a3c335a2f76107b44bb9a0a2eeab90a94cc79a
shangjinlong <shangjinlong@agora.io>
2026-09-11 17:46:28 +0800

    feat(media): unify prompt playback control

--- components/mybot/mybot_sdk/src/rtc/mybot_agora_rtc.c
+++ components/mybot/mybot_sdk/src/rtc/mybot_agora_rtc.c
@@ -742,7 +742,7 @@ int mybot_agora_rtc_init(const char *app_id, const mybot_agora_rtc_callbacks_t *
     rtc_service_option_t options;
     memset(&options, 0, sizeof(options));
     options.area_code = AREA_CODE_GLOB;
-    options.log_cfg.log_level = RTC_LOG_WARNING;
+    options.log_cfg.log_level = RTC_LOG_NOTICE;
     options.use_string_uid = true;
 
     int ret = agora_rtc_init((void *)app_id, &handler, &options);
